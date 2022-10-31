2439c1f1fdc9281d11a275519d1948a7140cd258
Ray Raspberry <ray@yfoundry.io>
2022-10-31 15:44:34 -0600

    Updated Start Script to work with 0.0.0.0 and direct Docker Builds

--- start_yfd_docs.sh
+++ start_yfd_docs.sh
@@ -2,7 +2,7 @@
 
 #bundle install
 #bundle update
-bundle exec jekyll serve --config _config_local.yml
+bundle exec jekyll serve --host=0.0.0.0 --config _config_local.yml
 #--port 80
 #--config _config_local.yml
 #bundle exec jekyll serve
